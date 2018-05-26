4e501f167096a243efc332f72af2bf2646ef224e
Bogomil Gospodinov <bgospodinov@outlook.com>
2018-05-26 19:44:43 +0100

    naive baseline for unseen and ambiguous tokens, .idea folder added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # msc_project
 University of Edinburgh MSc project 2017/2018
+
+## How to install
+1. Run `git submodule update --init` to initialise submodule dependencies
--- analysis/analyse_dataset.py
+++ analysis/analyse_dataset.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
 
 pd.set_option('display.max_rows', 50) 
 pd.set_option('display.max_columns', None)
@@ -30,13 +31,15 @@ if __name__ == "__main__":
     
     partition_dfs = {}
     partition_types = {}
-    
+
+    partition_dfs["all_tokens"] = {}
+
     for partition in config["DATASET"]["PARTITIONS"]:
         partition_df = pd.read_csv(os.path.join(args.folder, vars(args)[partition]), 
                     sep='\s+', 
                     names=cols)
         partition_df["word"] = partition_df["word"].str.lower()
-        partition_dfs[partition] = partition_df
+        partition_dfs["all_tokens"][partition] = partition_df
         
         types = {}
         for col in cols:
@@ -51,10 +54,16 @@ if __name__ == "__main__":
     
     for partition in config["DATASET"]["PARTITIONS"]:
         pt = partition_types[partition]
-        print(("{:<20}" * 5).format("{} set".format(partition), len(partition_dfs[partition].index), *[len(pt[c]) for c in cols]))
+        print(("{:<20}" * 5).format("{} set".format(partition), len(partition_dfs["all_tokens"][partition].index), *[len(pt[c]) for c in cols]))
     del partition, pt
     print("\n")
-    
+
+    # cache a subset of the partition dataframe of ambiguous word tokens and tag tokens, unseen during training
+    partition_dfs["unseen_word_tokens"] = {}
+    partition_dfs["unseen_lemma_tokens"] = {}
+    partition_dfs["unseen_tag_tokens"] = {}
+    partition_dfs["ambiguous_word_tokens"] = {}
+
     print(" Types unseen during training ".upper().center(TITLE_LENGTH, TITLE_CH))
     print(("{:<20}" * 5).format("", "", *["{} types".format(c).capitalize() for c in cols]))
     ptt = partition_types["training"]
@@ -62,19 +71,23 @@ if __name__ == "__main__":
         if partition != "training":
             pt = partition_types[partition]
             print(("{:<20}" * 5).format("{} set".format(partition), "", *[len(pt[c] - ptt[c]) for c in cols]))
-    del partition, pt, ptt     
-    print("\n")    
-    
+
+            # creating dataframes of unseen words during training for each partition of the dataset (i.e. valid and test sets)
+            for c in cols:
+                partition_dfs["unseen_{}_tokens".format(c)][partition] = partition_dfs["all_tokens"][partition]\
+                        [partition_dfs["all_tokens"][partition][c].isin(pt[c] - ptt[c])]
+    del partition, pt, ptt
+    print("\n")
+
     # ambiguity stats
     print(" Ambiguous lemma types with more than one ".upper().center(TITLE_LENGTH, TITLE_CH))
     print(("{:<20}" * 5).format("", "", "", *["{} type %".format(c).capitalize() for c in cols if c is not config["DATASET"]["COLUMNS"]["LEMMA"]]))
     for partition in config["DATASET"]["PARTITIONS"]:
-        pdf = partition_dfs[partition]
+        pdf = partition_dfs["all_tokens"][partition]
         print(("{:<20}" * 5).format("{} set".format(partition), "", "", *[pdf.groupby([config["DATASET"]["COLUMNS"]["LEMMA"]])[c].nunique().loc[lambda x: x > 1].size / pdf.groupby([config["DATASET"]["COLUMNS"]["LEMMA"]])[c].count().size for c in cols if c is not config["DATASET"]["COLUMNS"]["LEMMA"]]))
     del partition, pdf
     print("\n") 
 
-    
     print(" Lemma types with most wordforms in training set ".upper().center(TITLE_LENGTH, TITLE_CH))
     #print(partition_dfs["training"].groupby(["lemma"])["word"].nunique().loc[lambda x: x > 1].sort_values(ascending=False).head(20))
     print("\n") 
@@ -89,8 +102,16 @@ if __name__ == "__main__":
     print(" Ambiguous word types with more than one ".upper().center(TITLE_LENGTH, TITLE_CH))
     print(("{:<20}" * 5).format("", "", "", *["{} type %".format(c).capitalize() for c in cols if c is not config["DATASET"]["COLUMNS"]["WORD"]]))
     for partition in config["DATASET"]["PARTITIONS"]:
-        pdf = partition_dfs[partition]
+        pdf = partition_dfs["all_tokens"][partition]
         print(("{:<20}" * 5).format("{} set".format(partition), "", "", *[round(pdf.groupby([config["DATASET"]["COLUMNS"]["WORD"]])[c].nunique().loc[lambda x: x > 1].size / pdf.groupby([config["DATASET"]["COLUMNS"]["WORD"]])[c].count().size, 5) for c in cols if c is not config["DATASET"]["COLUMNS"]["WORD"]]))
+
+        # awk 'BEGIN { cnt=0; } { if ( tolower($1) == "български" ) { cnt++; print tolower($1), $2, $3; } } END { print cnt; }' training.txt | sort | uniq -c
+
+        # this line defines ambiguous word as having either more than one possible tag or one possible lemma
+        #ambiguous_index = pdf.groupby([config["DATASET"]["COLUMNS"]["WORD"]]).nunique().apply(lambda x: x > 1).any(axis=1)
+        ambiguous_index = pdf.groupby([config["DATASET"]["COLUMNS"]["WORD"]]).nunique()["lemma"] > 1
+        ambiguous_words = ambiguous_index.where(lambda x: x).dropna().index.tolist()
+        partition_dfs["ambiguous_word_tokens"][partition] = pdf[pdf["word"].isin(ambiguous_words)]
     del partition, pdf
     print("\n") 
     
@@ -104,34 +125,43 @@ if __name__ == "__main__":
     base_mem = {}        
     for col in cols:
         if col is not config["DATASET"]["COLUMNS"]["WORD"]:
-            base_mem[col] = partition_dfs["training"].groupby(["word", col]).size().to_frame("count")\
+            base_mem[col] = partition_dfs["all_tokens"]["training"].groupby(["word", col]).size().to_frame("count")\
                 .reset_index().sort_values(["word", "count"], ascending=[True, False])\
                 .drop_duplicates(subset="word").iloc[:, 0:2]
     del col
-    
-    print(("{:<20}" * 5).format("", "", "", *["{} acc %".format(c).capitalize() for c in cols if c is not config["DATASET"]["COLUMNS"]["WORD"]]))
-    
-    for partition in config["DATASET"]["PARTITIONS"]:
-        if partition != "training":
-            pdf = partition_dfs[partition]
-            x_test = pdf["word"]
-            acc = [] 
-            
-            for c in cols:
-                if c is not config["DATASET"]["COLUMNS"]["WORD"]:
-                    y_test = pdf[c]
-                    
-                    y_pred = x_test.to_frame().set_index('word')\
-                        .join(base_mem[c].set_index('word'), on=["word"])\
-                        .reset_index()
-                    
-                    if c == config["DATASET"]["COLUMNS"]["LEMMA"]:
-                        # interpolating lemmata
-                        y_pred[c] = y_pred[c].fillna(y_pred['word'])
-                    
-                    
-                    acc.append((y_pred[c] == y_test).value_counts(normalize=True).iloc[0])
-                    #y_pred[pd.isnull(y_pred).any(axis=1)]
-            
-            print(("{:<20}" * 5).format("{} set".format(partition), "", "", *acc))
-    del partition
+
+    for scope in partition_dfs.keys():
+        print(("{:<20}" * 6).format(scope.upper(), "", "Size", *["{} acc %".format(c).capitalize() for c in cols if c is not config["DATASET"]["COLUMNS"]["WORD"]], "Joint acc %"))
+
+        for partition in config["DATASET"]["PARTITIONS"]:
+            if partition != "training":
+                pdf = partition_dfs[scope][partition]
+                x_test = pdf["word"]
+                acc = []
+
+                # initializes the mask that keeps record of which predictions are correct for every column of the dataset
+                joint_mask = np.array([True] * pdf.shape[0])
+
+                for c in cols:
+                    if c is not config["DATASET"]["COLUMNS"]["WORD"]:
+                        y_test = pdf[c].reset_index(drop=True)
+
+                        y_pred = x_test.to_frame().set_index('word')\
+                            .join(base_mem[c].set_index('word'), on=["word"])\
+                            .reset_index()
+
+                        if c == config["DATASET"]["COLUMNS"]["LEMMA"]:
+                            # interpolating lemmata
+                            y_pred[c] = y_pred[c].fillna(y_pred['word'])
+
+                        pred_mask = (y_pred[c] == y_test)
+                        acc.append(1 - pred_mask.value_counts(normalize=True).loc[False])
+                        joint_mask &= pred_mask
+                        #y_pred[pd.isnull(y_pred).any(axis=1)]
+
+                # joint acc
+                acc.append(joint_mask.sum() / joint_mask.size)
+
+                print(("{:<20}" * 6).format("{} set".format(partition), "", pdf.shape[0], *acc))
+        del partition
+        print("\n")
