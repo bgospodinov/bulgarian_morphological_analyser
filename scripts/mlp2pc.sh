c1a9e4d95889b8455624ac502554c57b362a15df
Bogomil Gospodinov <bgospodinov@outlook.com>
2018-06-10 18:54:03 +0100

    fixes rsync script and py3 bugs in nematus inference

--- scripts/mlp2pc.sh
+++ scripts/mlp2pc.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 # usage: mlp2pc [path-on-mlp2] [local-pc-path] [include-path-on-mlp2]
 rsync -avzP -e 'ssh -o "ProxyCommand ssh -A s1312650@student.ssh.inf.ed.ac.uk nc %h %p"' \
-	--include=\'$3\' \
+	--include=$3 \
 	--exclude='*' \
 	s1312650@mlp2:$1 $2
