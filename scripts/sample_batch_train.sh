f6f4e9bd66a029ce0c057c309f43d5ca8e379489
Bogomil Gospodinov <bgospodinov@outlook.com>
2018-06-17 01:09:44 +0100

    training fixe

--- scripts/sample_batch_train.sh
+++ scripts/sample_batch_train.sh
@@ -10,12 +10,13 @@ declare -a attrs=(100 500 750 1000 1200)
 # 1 if the corresponding job should go to the LongJobs partition, 0 otherwise
 declare -a lj_pred=(0 0 0 1 1)
 
-for run in {1..${runs}} ; do
+for (( run=1; run<=$runs; run++ ))
+do
 	echo Run $run
 	iter=0
 	for attr in "${attrs[@]}" ; do
 		echo Launching\ attribute=$attr
-		context_size=$attr sbatch --output="logs/train-%j.out" --job-name="${run}-${attr}" $( (( ${lj_pred[$iter]} == 1 )) && printf %s '--partition="LongJobs"' ) train.sh
+		context_size=$attr sbatch --output="logs/train-%j.out" --job-name="${run}-${attr}" $( (( ${lj_pred[$iter]} == 1 )) && printf %s '--partition=LongJobs' ) train.sh
 		((iter++))
 	done
 done
