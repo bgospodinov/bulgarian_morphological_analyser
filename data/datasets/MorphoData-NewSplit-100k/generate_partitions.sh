b91c217d35d729d509d5fadee9637bddc5de0fd9
Bogomil Gospodinov <bgospodinov@outlook.com>
2018-07-20 15:07:07 +0100

    fixes 100k dataset

--- data/datasets/MorphoData-NewSplit-100k/generate_partitions.sh
+++ data/datasets/MorphoData-NewSplit-100k/generate_partitions.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-for f in train dev test ; do 
-	awk '{ print $2, $3, $4 }' <(cat $f/*) > ${f}.txt
+for f in train dev ; do 
+	cat $f/* | expand > ${f}.txt
 	dos2unix ${f}.txt
+	awk 'NF > 0 {blank=0} NF == 0 {blank++} blank < 2 && NR>1 { print $2, $3, $4 }' ${f}.txt > ${f}.tmp.txt
+	rm -f ${f}.txt
+	mv ${f}.tmp.txt ${f}.txt
+	sed -i.bak -r -e 's/^\s+$//g' ${f}.txt
 done
 
 mv train.txt training.txt
+rm -f *.bak
